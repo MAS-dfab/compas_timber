3bf0e48781fc0d94a0ed153be835cf4fb4ffacf7
papachap <ppapachar@gmail.com>
2024-05-21 11:39:15 +0200

    implement flip_lap condition for frl

--- src/compas_timber/fabrication/joint_factories/french_ridge_factory.py
+++ src/compas_timber/fabrication/joint_factories/french_ridge_factory.py
@@ -3,6 +3,8 @@ from compas_timber.fabrication import BTLx
 from compas_timber.fabrication import BTLxFrenchRidgeLap
 from compas_timber.fabrication.btlx_processes.btlx_drilling import BTLxDrilling
 
+from compas.geometry import cross_vectors, dot_vectors
+
 
 class FrenchRidgeFactory(object):
     """
@@ -32,10 +34,18 @@ class FrenchRidgeFactory(object):
 
         top_key = joint.beams[0].key
         top_part = parts[str(top_key)]
-        top_part.processings.append(BTLxFrenchRidgeLap.create_process(top_part, joint, True, joint.drill_diameter))
 
         bottom_key = joint.beams[1].key
         bottom_part = parts[str(bottom_key)]
+
+        # define a process for choosing top part
+        top_vect = top_part.beam.centerline.direction
+        compare_vect = cross_vectors(top_vect, [0, 0, 1])
+        if dot_vectors(top_vect, compare_vect) == 0:
+            top_part, bottom_part = bottom_part, top_part
+
+
+        top_part.processings.append(BTLxFrenchRidgeLap.create_process(top_part, joint, True, joint.drill_diameter))
         bottom_part.processings.append(BTLxFrenchRidgeLap.create_process(bottom_part, joint, False, 0.0))
 
 
